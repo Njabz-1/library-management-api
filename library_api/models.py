ea52505848a8a3578c358afe517f3aa882b5abcf
Njabulo <mhlambinjabulo@gmail.com>
2024-10-14 20:40:17 +0200

    add initial models

--- library_api/models.py
+++ library_api/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+class User(AbstractUser):
+    date_of_membership = models.DateField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.username
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    isbn = models.CharField(max_length=13, unique=True)
+    published_date = models.DateField()
+    copies_available = models.PositiveIntegerField(validators=[MinValueValidator(0)])
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
+class Transaction(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='transactions')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='transactions')
+    checkout_date = models.DateTimeField(auto_now_add=True)
+    return_date = models.DateTimeField(null=True, blank=True)
+    is_returned = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.book.title}"
