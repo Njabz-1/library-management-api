73b0d67ab9f4848eb2bb3f6dcbdbbf4ad1f7fe83
Njabulo <mhlambinjabulo@gmail.com>
2024-10-15 20:55:12 +0200

    Return a book function was not working as I was getting NameError with regards to timezone. Add import for timezone

--- library_api/views.py
+++ library_api/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from .models import User, Book, Transaction
 from .serializers import UserSerializer, BookSerializer, TransactionSerializer
+from django.utils import timezone
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
